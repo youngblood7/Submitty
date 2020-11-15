59c2095f17b136e84baf33aec05284ad696cdb8e
Shail Patel <shailpatel67@gmail.com>
2020-11-14 20:47:42 -0500

    debug

--- .setup/testing/setup.sh
+++ .setup/testing/setup.sh
@@ -75,7 +75,7 @@ bash -c "echo 'export PATH=${PATH}' >> /home/${DAEMON_USER}/.bashrc"
 bash -c "echo 'export PATH=${PATH}' >> /home/${DAEMON_USER}/.bashrc"
 
 # necessary to pass config path as submitty_repository is a symlink
-python3 ${SUBMITTY_REPOSITORY}/migration/run_migrator.py -e master -e system migrate --initial
+python3 /migration/run_migrator.py -e master -e system migrate --initial
 
 bash ${SUBMITTY_INSTALL_DIR}/.setup/INSTALL_SUBMITTY.sh clean skip_web_restart
 
--- migration/run_migrator.py
+++ migration/run_migrator.py
@@ -6,4 +6,10 @@ from migrator import cli
 if __name__ == '__main__':
     config_path = Path(Path(__file__).parent.resolve(), '..', '..', '..', 'config')
     config_path = config_path.resolve() if config_path.exists() else None
+
+    print("debug")
+    print(sys.argv)
+    print(config_path)
+    print(sys.argv[1:])
+    
     cli.run(sys.argv[1:], config_path)
