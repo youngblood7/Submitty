018fdae6223e988c88df366e13f34ac275bd4ae3
Shail Patel <shailpatel67@gmail.com>
2020-11-16 09:20:48 -0500

    fix missing env var

--- .setup/INSTALL_SUBMITTY_HELPER.sh
+++ .setup/INSTALL_SUBMITTY_HELPER.sh
@@ -36,6 +36,9 @@ if [ -z ${SUBMITTY_REPOSITORY} ] && [ -z ${SUBMITTY_INSTALL_DIR} ]; then
     SUBMITTY_REPOSITORY=$(jq -r '.submitty_repository' ${CONF_DIR}/submitty.json)
     SUBMITTY_INSTALL_DIR=$(jq -r '.submitty_install_dir' ${CONF_DIR}/submitty.json)
     source ${THIS_DIR}/bin/versions.sh
+else
+    source $SUBMITTY_REPOSITORY/.setup/bin/versions
+    echo $AnalysisTools_Version
 fi
 
 if [ ${WORKER} == 0 ]; then
