da3aaef47dc2bb946a4c1e4a0f7a59a41eac5b4f
Vikram Srikishan <vikrsri@gmail.com>
2020-12-21 15:11:59 -0500

    started migration

---
+++ migration/migrator/migrations/course/20201221061821_registration_subsection.py
@@ -0,0 +1,35 @@
+"""Migration for a given Submitty course database."""
+
+
+def up(config, database, semester, course):
+    """
+    Run up migration.
+
+    :param config: Object holding configuration details about Submitty
+    :type config: migrator.config.Config
+    :param database: Object for interacting with given database for environment
+    :type database: migrator.db.Database
+    :param semester: Semester of the course being migrated
+    :type semester: str
+    :param course: Code of course being migrated
+    :type course: str
+    """
+    database.execute("ALTER TABLE ONLY users ALTER COLUMN registration_subsection SET DATA TYPE character varying(255) USING registration_subsection::varchar(255)")
+
+
+
+def down(config, database, semester, course):
+    """
+    Run down migration (rollback).
+
+    :param config: Object holding configuration details about Submitty
+    :type config: migrator.config.Config
+    :param database: Object for interacting with given database for environment
+    :type database: migrator.db.Database
+    :param semester: Semester of the course being migrated
+    :type semester: str
+    :param course: Code of course being migrated
+    :type course: str
+    """
+    database.execute("ALTER TABLE ONLY users ALTER COLUMN registration_subsection SET DATA TYPE integer USING registration_subsection::integer")
+    pass
