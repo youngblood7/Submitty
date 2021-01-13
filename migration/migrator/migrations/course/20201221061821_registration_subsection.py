af651c398faab997cf009de4922ec261450afc5f
Vikram Srikishan <vikrsri@gmail.com>
2021-01-13 03:28:47 -0500

    minor change

--- migration/migrator/migrations/course/20201221061821_registration_subsection.py
+++ migration/migrator/migrations/course/20201221061821_registration_subsection.py
@@ -1,3 +1,3 @@
 
 def up(config, database, semester, course):
-    database.execute("ALTER TABLE ONLY users ALTER COLUMN registration_subsection SET DATA TYPE character varying(255) USING users.registration_subsection::varchar(255)")
+    database.execute("ALTER TABLE ONLY users ALTER COLUMN registration_subsection SET DATA TYPE character varying(255) USING registration_subsection::varchar(255)")
