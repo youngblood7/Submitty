7039239d1ed9aa9ab9ff5bd350cc0a749d32bd7e
Vikram Srikishan <vikrsri@gmail.com>
2021-01-12 22:57:36 -0500

    change migration

--- migration/migrator/migrations/course/20201221061821_registration_subsection.py
+++ migration/migrator/migrations/course/20201221061821_registration_subsection.py
@@ -1,10 +1,3 @@
-"""Migration for a given Submitty course database."""
-
 
 def up(config, database, semester, course):
     database.execute("ALTER TABLE ONLY users ALTER COLUMN registration_subsection SET DATA TYPE character varying(255) USING registration_subsection::varchar(255)")
-
-
-
-def down(config, database, semester, course):
-    database.execute("ALTER TABLE ONLY users ALTER COLUMN registration_subsection SET DATA TYPE integer USING registration_subsection::integer")
